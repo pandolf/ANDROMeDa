13c5842283a3c3be7eb8943138b7bbb9ef57b417
pandolf <francesco.pandolfi@cern.ch>
2021-12-20 16:08:52 +0100

    added fit line

--- FieldEmissionAnalysis/analysis/drawFieldEmission.cpp
+++ FieldEmissionAnalysis/analysis/drawFieldEmission.cpp
@@ -1,14 +1,18 @@
 #include "AmdCommon.h"
 
+#include <iostream>
 
 #include "TGraphErrors.h"
 #include "TCanvas.h"
 #include "TH2D.h"
+#include "TMath.h"
+#include "TF1.h"
 
 
 
 void addPoint( TGraphErrors* gr, float hv, std::vector<float> i_meas );
 void getMeanRMS( std::vector<float> v, float& mean, float& rms );
+TGraphErrors* getFNgraph( TGraphErrors* g );
 
 
 int main() {
@@ -40,6 +44,43 @@ int main() {
   graph->Draw("P same");
 
   c1->SaveAs( "iv.pdf" );
+
+  c1->Clear();
+  c1->SetLogy();
+
+  TH2D* h2_axes_log = new TH2D( "axes_log", "", 10, 0., 2000., 10, 0.1, 2000. );
+  h2_axes_log->SetXTitle( "V (V)" );
+  h2_axes_log->SetYTitle( "I (nA)" );
+  h2_axes_log->Draw();
+
+  graph->Draw("P same");
+
+  c1->SaveAs( "iv_log.pdf" );
+
+
+  TGraphErrors* gr_fn = getFNgraph( graph );
+
+  TCanvas* c1_fn = new TCanvas( "c1_fn", "", 600, 600 );
+  c1_fn->Clear();
+
+  TH2D* h2_axes_fn = new TH2D( "axes_fn", "", 10, 0.0005, 0.0011, 10, -15., -5. );
+  h2_axes_fn->SetXTitle( "1/V (V^{-1})" );
+  h2_axes_fn->SetYTitle( "Log(I/V^{2}) (a.u.)" );
+  h2_axes_fn->Draw();
+
+  gr_fn->SetMarkerStyle(20);
+  gr_fn->SetMarkerSize(1.5);
+  gr_fn->SetMarkerColor(kGray+3);
+  gr_fn->SetLineColor  (kGray+3);
+
+  gr_fn->Draw("P same");
+
+  TF1* f1_line = new TF1( "line", "[0]+[1]*x" );
+  f1_line->SetLineColor(46);
+  f1_line->SetLineWidth(2);
+  gr_fn->Fit( f1_line );
+
+  c1_fn->SaveAs("fn.pdf");
 
   return 0;
 
@@ -77,3 +118,28 @@ void getMeanRMS( std::vector<float> v, float& mean, float& rms ) {
   rms = sqrt(rms);
 
 }
+
+
+
+TGraphErrors* getFNgraph( TGraphErrors* g ) {
+
+  TGraphErrors* gr_fn = new TGraphErrors(0);
+  gr_fn->SetName( "gr_fn" );
+
+  for( unsigned iPoint=0; iPoint<g->GetN(); ++iPoint ) {
+
+    double i, v;
+    g->GetPoint(iPoint, v, i);
+    float i_err = g->GetErrorY( iPoint );
+    float v_err = 1.;
+
+    gr_fn->SetPoint     ( iPoint, 1./v, TMath::Log( i / (v*v) ) );
+    std::cout << 1./v << " " <<  TMath::Log( i / (v*v) ) << std::endl;
+    gr_fn->SetPointError( iPoint, v_err/(v*v), i_err/i );
+
+  } // for
+
+  return gr_fn;
+
+}
+
