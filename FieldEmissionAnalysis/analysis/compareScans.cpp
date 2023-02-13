d1b1b129884c21f557213f902fca65bcf04320ac
pandolf <francesco.pandolfi@cern.ch>
2023-02-13 14:45:12 +0100

    flexible scale (taken from name), saves univocal files for default comparisons

--- FieldEmissionAnalysis/analysis/compareScans.cpp
+++ FieldEmissionAnalysis/analysis/compareScans.cpp
@@ -7,12 +7,13 @@
 #include "TCanvas.h"
 #include "TLegend.h"
 #include "TH2D.h"
+#include "TString.h"
 
 
 
 
 TGraphErrors* getGraph( const std::string& name, float scale=1. );
-
+float getScale( const std::string& name );
 
 
 
@@ -22,19 +23,24 @@ int main( int argc, char* argv[] ) {
 
   std::string saveName;
   std::string scan1(""), scan2("");
-  float scale1(1.), scale2(1.);
+  float scale1(1.);
+  float scale2(1.);
   std::string legend1("Scan 1"), legend2("Scan 2");
-  float ymax = 20.;
+  float ymin = -1.;
+  float ymax = 9.;
+  bool logy = false;
+  float textSize = 0.035;
   
   if( argc == 3 ) {
 
     scan1 = std::string(argv[1]);
     scan2 = std::string(argv[2]);
-    scale1 = 1.;
-    scale2 = 1.;
-    legend1 = "Scan 1";
-    legend2 = "Scan 2";
+    scale1 = getScale(scan1);
+    scale2 = getScale(scan2);
+    legend1 = scan1;
+    legend2 = scan2;
     saveName = "";
+    textSize = 0.025;
 
   } else if( argc == 2 ) {
 
@@ -55,9 +61,45 @@ int main( int argc, char* argv[] ) {
       scan2 = "CNTetchedOLD_N6bis_whopper_d1_20230208_posDeltaV";
       scale1 = +1.;
       scale2 = -1.;
-      legend1 = "Negative";
-      legend2 = "Positive";
-      ymax = 2000.;
+      legend1 = "CNT HV < 0";
+      legend2 = "CNT HV > 0";
+      logy = true;
+
+    } else if( saveName=="ionOFF" ) {
+
+      scan1 = "CNTetchedOLD_N6bis_whopper_d1_20230208_posDeltaV";
+      scan2 = "CNTetchedOLD_N6bis_whopper_d1_20230209_posDeltaV_ionOFF";
+      scale1 = -1.;
+      scale2 = -1.;
+      legend1 = "Ion gauge on";
+      legend2 = "Ion gauge off";
+
+    } else if( saveName=="doubleTeflon" ) {
+
+      scan1 = "CNTetchedOLD_N6bis_whopper_d1_20230208_posDeltaV";
+      scan2 = "CNTetchedOLD_N6bis_whopper_d2_20230209_posDeltaV";
+      scale1 = -1.;
+      scale2 = -1.;
+      legend1 = "d = 1 mm";
+      legend2 = "d = 2 mm";
+
+    } else if( saveName=="tripleTeflon" ) {
+
+      scan1 = "CNTetchedOLD_N6bis_whopper_d1_20230208_posDeltaV";
+      scan2 = "CNTetchedOLD_N6bis_whopper_d3_20230209_posDeltaV";
+      scale1 = -1.;
+      scale2 = -1.;
+      legend1 = "d = 1 mm";
+      legend2 = "d = 3 mm";
+
+    } else if( saveName=="posNegAfterClean" ) {
+
+      scan1 = "CNTetchedOLD_N6bis_whopper_d1p5_20230213";
+      scan2 = "CNTetchedOLD_N6bis_whopper_d1p5_20230213_posDeltaV";
+      scale1 = +1.;
+      scale2 = -1.;
+      legend1 = "HV < 0";
+      legend2 = "HV > 0";
 
     } // if saveName
 
@@ -68,6 +110,12 @@ int main( int argc, char* argv[] ) {
 
   } // if argc
 
+
+  std::cout << std::endl;
+  std::cout << "-> Will compare scans:" << std::endl;
+  std::cout << "   " << scan1 << "   with scale: " << scale1 << std::endl;
+  std::cout << "   " << scan2 << "   with scale: " << scale2 << std::endl;
+  std::cout << std::endl;
 
   
   TGraphErrors* gr0 = getGraph( scan1, scale1 );
@@ -85,29 +133,52 @@ int main( int argc, char* argv[] ) {
 
   TCanvas* c1 = new TCanvas( "c1", "c1", 600, 600 );
   c1->cd();
-  c1->SetLogy();
 
-  TH2D* h2_axes = new TH2D( "axes", "", 10, 0., 1100., 10, 0.1, ymax );
+  TH2D* h2_axes = new TH2D( "axes", "", 10, 0., 1100., 10, ymin, ymax );
   h2_axes->SetXTitle( "|#DeltaV| (V)" );
   h2_axes->SetYTitle( "I (pA)" );
   h2_axes->Draw();
 
+  
+  TCanvas* c1_log = new TCanvas( "c1_log", "c1_log", 600, 600 );
+  c1_log->cd();
+  c1_log->SetLogy();
+
+  TH2D* h2_axes_log = new TH2D( "axes_log", "", 10, 0., 1100., 10, 0.1, 2000. );
+  h2_axes_log->SetXTitle( "|#DeltaV| (V)" );
+  h2_axes_log->SetYTitle( "I (pA)" );
+  h2_axes_log->Draw();
+
   TLegend* legend = new TLegend( 0.2, 0.72, 0.55, 0.87 );
-  legend->SetTextSize( 0.035 );
+  legend->SetTextSize( textSize );
   legend->SetFillColor( 0 );
   legend->AddEntry( gr0, legend1.c_str(), "P" );
   legend->AddEntry( gr1, legend2.c_str(), "P" );
-  legend->Draw("same");
 
+  c1->cd();
+  legend->Draw("same");
   gr0->Draw("Psame");
   gr1->Draw("Psame");
 
   gPad->RedrawAxis();
 
   if( saveName=="" ) 
-    c1->SaveAs( "compareScansIV.pdf");
+    c1->SaveAs( Form("plots/compareScansIV_%s_vs_%s.pdf", scan1.c_str(), scan2.c_str()) );
   else 
-    c1->SaveAs( Form("compareScansIV_%s.pdf", saveName.c_str()) );
+    c1->SaveAs( Form("plots/compareScansIV_%s.pdf", saveName.c_str()) );
+   
+
+  c1_log->cd();
+  legend->Draw("same");
+  gr0->Draw("Psame");
+  gr1->Draw("Psame");
+
+  gPad->RedrawAxis();
+
+  if( saveName=="" ) 
+    c1_log->SaveAs( Form("plots/compareScansIV_%s_vs_%s_log.pdf", scan1.c_str(), scan2.c_str()) );
+  else 
+    c1_log->SaveAs( Form("plots/compareScansIV_%s_log.pdf", saveName.c_str()) );
    
 
   return 0;
@@ -139,5 +210,17 @@ TGraphErrors* getGraph( const std::string& name, float scale ) {
   } // for points
 
   return returnGraph;
+
+}
+
+
+
+float getScale( const std::string& name ) {
+
+  TString tstr(name);
+  
+  float scale = (tstr.Contains("posDeltaV") || tstr.Contains("drain")) ? -1. : 1.;
+
+  return scale;
 
 }
