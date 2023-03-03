33a8f70a82aa9480a1e01f4c06f62b2128acbcae
pandolf <francesco.pandolfi@cern.ch>
2023-03-03 11:58:48 +0100

    latest version

--- FieldEmissionAnalysis/src/IVScanFN.cc
+++ FieldEmissionAnalysis/src/IVScanFN.cc
@@ -115,12 +115,14 @@ void IVScanFN::set_graphFN() {
     graphFN_->Fit( f1_line, "Q+" );
 
     float phi = 4.7; // in eV
+    float phi_err = 0.1; // in eV
     float d = this->d(); // in mm
-    float d_err = 0.1; // see logbook_ANDROMeDa entry 24/01/22 for details on why 0.1 mm
+    float d_err = 0.2; // 0.1 for the syst on the position (linear shifter, see logbook_ANDROMeDa entry 24/01/22) plus 0.1 for the uncertainty on the length of the tubes
     float s = f1_line->GetParameter(1);
     float s_err = f1_line->GetParError(1);
-    gamma_ = -6.83E6*phi*sqrt(phi)*d/s;
-    gamma_err_ = sqrt( gamma_*gamma_/(s*s)*s_err*s_err + gamma_*gamma_/(d*d)*d_err*d_err );
+    float b = 6.83E6; // this is 4/3 * sqrt(2m) / hbar = 6.83E6 V^-1/2 mm^-1, from FN theory
+    gamma_ = -b*phi*sqrt(phi)*d/s; 
+    gamma_err_ = sqrt( gamma_*gamma_/(s*s)*s_err*s_err + gamma_*gamma_/(d*d)*d_err*d_err + 9.*gamma_*gamma_/(4.*phi*phi)*phi_err*phi_err ); // propagazione
 
   }
 
