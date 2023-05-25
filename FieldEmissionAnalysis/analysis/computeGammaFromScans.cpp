d10e50939a3060c7baa7e6c1d50e348f23d06eb4
pandolf <francesco.pandolfi@cern.ch>
2023-05-25 15:25:39 +0200

    steps

--- FieldEmissionAnalysis/analysis/computeGammaFromScans.cpp
+++ FieldEmissionAnalysis/analysis/computeGammaFromScans.cpp
@@ -37,13 +37,10 @@ int main( int argc, char* argv[] ) {
 
   } else if( sampleName == "CNTetchedOLD_N1new" ) {
 
-    scans.push_back( "CNTetchedOLD_N1new_d3_20230515.dat" );
-    scans.push_back( "CNTetchedOLD_N1new_d3_20230516.dat" );
-    scans.push_back( "CNTetchedOLD_N1new_d4_20230516.dat" );
-    scans.push_back( "CNTetchedOLD_N1new_d3p3_20230517_drain.dat" );
-    scans.push_back( "CNTetchedOLD_N1new_d4p3_20230517_drain.dat" );
-    scans.push_back( "CNTetchedOLD_N1new_d4p3_20230517_drain_2.dat" );
+    //scans.push_back( "CNTetchedOLD_N1new_d3p3_20230517_drain.dat" );
+    //scans.push_back( "CNTetchedOLD_N1new_d4p3_20230517_drain_2.dat" );
     scans.push_back( "CNTetchedOLD_N1new_d4_20230517_drain.dat" );
+    scans.push_back( "CNTetchedOLD_N1new_d4p3_20230517_drain.dat" );
     scans.push_back( "CNTetchedOLD_N1new_d5_20230517_drain.dat" );
 
   } else if( sampleName == "CNTetchedOLD_N2new" ) {
@@ -62,9 +59,9 @@ int main( int argc, char* argv[] ) {
 
   } else if( sampleName == "CNTetchedOLD_N3new" ) {
 
+    scans.push_back( "CNTetchedOLD_N3new_d3_20230519_drain.dat" );
     scans.push_back( "CNTetchedOLD_N3new_d4_20230519_drain.dat" );
     scans.push_back( "CNTetchedOLD_N3new_d5_20230519_drain.dat" );
-    scans.push_back( "CNTetchedOLD_N3new_d3_20230519_drain.dat" );
 
   } else {
 
@@ -73,44 +70,95 @@ int main( int argc, char* argv[] ) {
 
   }
 
+
+  std::string outdir("plots/GammaScans/"+sampleName);
+  system( Form("mkdir -p %s", outdir.c_str()) );
+
+
   AndCommon::setStyle();
 
   std::vector<int> colors = AndCommon::colors();
 
 
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
-  c1->cd();
+  TCanvas* c2 = new TCanvas( "c2", "", 600, 600 );
+
 
   TH2D* h2_axes = new TH2D( "axes", "", 10, 0., 2000., 10, 0., 30. );
   h2_axes->SetXTitle( "#DeltaV [V]" );
   h2_axes->SetYTitle( "I [#muA]" );
-  h2_axes->Draw();
 
-  TLegend* legend = new TLegend( 0.2, 0.65, 0.5, 0.9, sampleName.c_str() );
-  legend->SetTextSize(0.035);
-  legend->SetFillColor(0);
+  TH2D* h2_axesE = new TH2D( "axesE", "", 10, 0., 400., 10, 0., 30. );
+  h2_axesE->SetXTitle( "#DeltaV / d [V/mm]" );
+  h2_axesE->SetYTitle( "I [#muA]" );
 
-  for( unsigned i=0; i<scans.size(); ++i ) {
 
-    IVScan scan(scans[i]);
+  int nsteps = 20;
+  int startstep = 0;
+  float stepsize = 0.1; // in mm
 
-    scan.scaleDataPoints( 1./1000000. ); // in microA
+  for( int istep=startstep; istep<nsteps; ++istep ) {
 
-    TGraphErrors* graph = scan.graph();
+    c1->Clear();
+    c2->Clear();
 
-    graph->SetMarkerColor( colors[i] );
-    graph->SetLineColor( colors[i] );
-    graph->Draw( "P same" );
+    c1->cd();
+    h2_axes->Draw();
 
-    legend->AddEntry( graph, Form("d = %.1f mm", scan.d()), "P" );
+    c2->cd();
+    h2_axesE->Draw();
 
-  }
+    TLegend* legend = new TLegend( 0.2, 0.65, 0.5, 0.9, sampleName.c_str() );
+    legend->SetTextSize(0.035);
+    legend->SetFillColor(0);
 
-  legend->Draw("same");
-  
-  gPad->RedrawAxis();
 
-  c1->SaveAs( "test.pdf" );
+    for( unsigned i=0; i<scans.size(); ++i ) {
+
+      IVScan scan(scans[i]);
+
+      scan.scaleDataPoints( 1./1000000. ); // in microA
+
+      scan.set_d( scan.d() + istep*stepsize );
+
+      if( istep==0 ) { // only once
+
+        c1->cd();
+        TGraphErrors* graph = scan.graph();
+        graph->SetMarkerSize( 1.6 );
+        graph->SetMarkerColor( colors[i] );
+        graph->SetLineColor( colors[i] );
+        graph->Draw( "P same" );
+
+      } // if step =0 
+
+      c2->cd();
+      TGraphErrors* graph_vsE = scan.graph_vsE();
+      graph_vsE->SetMarkerSize( 1.6 );
+      graph_vsE->SetMarkerColor( colors[i] );
+      graph_vsE->SetLineColor( colors[i] );
+      graph_vsE->Draw( "P same" );
+
+      legend->AddEntry( graph_vsE, Form("d = %.1f mm", scan.d()), "P" );
+
+    }
+
+    if( istep==0 ) { // only once
+      c1->cd();
+      legend->Draw("same");
+      gPad->RedrawAxis();
+      c1->SaveAs( Form("%s/i_vs_v.pdf", outdir.c_str()) );
+    }
+
+    c2->cd();
+    legend->Draw("same");
+    gPad->RedrawAxis();
+    c2->SaveAs( Form("%s/i_vs_e_step%d.pdf", outdir.c_str(), istep) );
+
+    delete legend;
+
+  } // steps
+
 
   return 0;
 
