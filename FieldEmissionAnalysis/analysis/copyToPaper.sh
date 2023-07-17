7eb8b68fd889ea8f37ae1302fc1a6a2da5af4377
pandolf <francesco.pandolfi@cern.ch>
2023-07-17 16:14:07 +0200

    first commit

---
+++ FieldEmissionAnalysis/analysis/copyToPaper.sh
@@ -0,0 +1,5 @@
+cp plots/GammaScans/CNTArO2Etching_AG_old/i_vs_v.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/i_vs_v_CNTArO2Etching_AG_old.pdf
+cp plots/GammaScans/CNTArO2Etching_N1/i_vs_v.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/i_vs_v_CNTArO2Etching_N1.pdf
+cp plots/GammaScans/CNTArO2Etching_AG_old/fn.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/fn_CNTArO2Etching_AG_old.pdf
+cp plots/GammaScans/CNTArO2Etching_N1/fn.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/fn_CNTArO2Etching_N1.pdf
+cp plots/GammaScans/CNTArO2Etching_N1/chi2Scan.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/chi2Scan_CNTArO2Etching_N1.pdf
