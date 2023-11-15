8fa671557658be6a8d799b460fddb077d978b315
pandolf <francesco.pandolfi@cern.ch>
2023-11-15 16:34:59 +0100

    recoded class, now only iMax and iMin are data members, everything else (graphFN and gamma) is returned

--- FieldEmissionAnalysis/interface/IVScanFN.h
+++ FieldEmissionAnalysis/interface/IVScanFN.h
@@ -13,18 +13,19 @@ class IVScanFN : public IVScan {
 
  public:
 
-  IVScanFN( const std::string& name, float scale=1., float vMin = -99999., float vMax = 99999. );
+  IVScanFN( const std::string& name, float scale=1., float iMin=0., float iMax=1E9 );   //, float vMin = -99999., float vMax = 99999. );
 
   ~IVScanFN();
 
-  TGraphErrors* getFNgraph() const;
-  static TGraphErrors* getFNgraph( const IScan* scan );
-  static TGraphErrors* getFNgraph( TGraphErrors* graph, float vMin = -99999., float vMax = 99999., int n = 1, float verr = 0.1 );
-
   TGraphErrors* graphFN() const;
-  TF1* lineFN() const;
+  static TGraphErrors* graphFN( const IScan* scan, float iMin = 0., float iMax = 1E9 );
+  static TGraphErrors* graphFN( TGraphErrors* graph, float iMin = 0., float iMax = 1E9, int n = 1, float verr = 0.1 );
 
-  static float get_gamma_and_err( float& gamma_err_uncorr, float& gamma_err_corr, float s, float s_err, float d, float d_err=-1. );
+  //TGraphErrors* graphFN() const;
+  //TF1* lineFN() const;
+
+  float get_gamma_and_err( float& gamma_err_uncorr, float& gamma_err_corr, float d_errcorr=-1. );
+  static float get_gamma_and_err( float& gamma_err_uncorr, float& gamma_err_corr, float s, float s_err, float d, float d_errcorr=-1. );
 
   static float phi();
   static float phi_err();
@@ -32,11 +33,17 @@ class IVScanFN : public IVScan {
   static float d_err_uncorr();
   static float d_err_corr();
 
-  float gamma() const;
-  float gamma_err() const;
+  float iMin() const;
+  float iMax() const;
 
-  virtual void set_graph( TGraphErrors* graph );
-  virtual void setColor( int color );
+  //float gamma() const;
+  //float gamma_err() const;
+
+  void set_iMin( float iMin );
+  void set_iMax( float iMax );
+  void set_iMinMax( float iMin, float iMax );
+
+  //virtual void setColor( int color );
 
   static std::string xTitleFN();
   static std::string yTitleFN();
@@ -45,12 +52,15 @@ class IVScanFN : public IVScan {
 
  private:
 
-  TGraphErrors* graphFN_; // F-N graph: Log(I/V^2) vs 1/V
+  //TGraphErrors* graphFN_; // F-N graph: Log(I/V^2) vs 1/V
 
-  void set_graphFN();
+  //void set_graphFN();
 
-  float gamma_; // field enhancement factor
-  float gamma_err_;
+  float iMin_;
+  float iMax_;
+
+  //float gamma_; // field enhancement factor
+  //float gamma_err_;
 
 
 };
--- FieldEmissionAnalysis/src/IVScanFN.cc
+++ FieldEmissionAnalysis/src/IVScanFN.cc
@@ -9,136 +9,94 @@
 #include "TString.h"
 #include "TMath.h"
 #include "TF1.h"
+#include "TFile.h"
 
 
 
-IVScanFN::IVScanFN( const std::string& name, float scale, float xMin, float xMax ) : IVScan( name, scale, xMin, xMax ) {
+IVScanFN::IVScanFN( const std::string& name, float scale, float iMin, float iMax ) : IVScan( name, scale ) {
 
-  gamma_ = 0.;
-  gamma_err_ = 0.;
-
-  // set FN graph:
-  set_graphFN();
-
-  setColor(46);
+  iMin_ = iMin;
+  iMax_ = iMax;
 
 }
 
 
 IVScanFN::~IVScanFN() {
 
-  //delete graphFN_;
-  //graphFN_ = 0;
+}
+
+
+
+float IVScanFN::iMin() const {
+
+  return iMin_;
+
+}
+
+
+float IVScanFN::iMax() const {
+
+  return iMax_;
 
 }
 
 
 
-TGraphErrors* IVScanFN::graphFN() const {
+void IVScanFN::set_iMin( float iMin ) {
 
-  return graphFN_;
+  iMin_ = iMin;
+
+}
+
+
+void IVScanFN::set_iMax( float iMax ) {
+
+  iMax_ = iMax;
+
+}
+
+
+void IVScanFN::set_iMinMax( float iMin, float iMax ) {
+
+  iMin_ = iMin;
+  iMax_ = iMax;
 
 }
 
 
 
-float IVScanFN::gamma() const {
+TGraphErrors* IVScanFN::graphFN() const {  
 
-  return gamma_;
-
-}
-
-
-float IVScanFN::gamma_err() const {
-
-  return gamma_err_;
-
-}
-
-
-void IVScanFN::setColor( int color ) {
-
-  IVScan::setColor(color);
-
-  graphFN_->SetMarkerColor( color );
-  graphFN_->SetLineColor( color );
-
-  graphFN_->GetFunction( Form("lineFN_%s", this->name().c_str()) )->SetLineColor( color );
+  return IVScanFN::graphFN( this->graph(), this->iMin(), this->iMax(), this->n(), this->verr() );
 
 }
 
 
 
-void IVScanFN::set_graph( TGraphErrors* graph ) {
+TGraphErrors* IVScanFN::graphFN( const IScan* scan, float iMin, float iMax ) {
 
-  IVScan::set_graph(graph);
-
-  set_graphFN();
+  return IVScanFN::graphFN( scan->graph(), iMin, iMax, scan->n(), scan->verr() );
 
 }
 
 
 
 
-void IVScanFN::set_graphFN() {
+TGraphErrors* IVScanFN::graphFN( TGraphErrors* graph, float iMin, float iMax, int n, float verr ) {
 
-  //if( graphFN_ != 0 ) delete graphFN_;
-
-  graphFN_ = IVScanFN::getFNgraph();
-
-
-  if( graphFN_->GetN() > 1 ) {
-
-    TF1* f1_line = new TF1( Form("lineFN_%s", this->name().c_str()), "[0]+[1]*x" );
-    f1_line->SetLineColor(46);
-    f1_line->SetLineWidth(2);
-    graphFN_->Fit( f1_line, "Q+" );
-
-    float gamma_err_uncorr, gamma_err_corr;
-    gamma_ = this->get_gamma_and_err( gamma_err_uncorr, gamma_err_corr, f1_line->GetParameter(1), f1_line->GetParError(1), this->d() );
-
-    gamma_err_ = sqrt( gamma_err_uncorr*gamma_err_uncorr + gamma_err_corr*gamma_err_corr );
-
-//  float phi = 4.7; // in eV
-//  float phi_err = 0.1; // in eV
-//  float d = this->d(); // in mm
-//  float d_err = 0.2; // 0.1 for the syst on the position (linear shifter, see logbook_ANDROMeDa entry 24/01/22) plus 0.1 for the uncertainty on the length of the tubes
-//  float s = f1_line->GetParameter(1);
-//  float s_err = f1_line->GetParError(1);
-//  float b = 6.83E6; // this is 4/3 * sqrt(2m) / hbar = 6.83E6 V^-1/2 mm^-1, from FN theory
-//  //gamma_ = -b*phi*sqrt(phi)*d/s; 
-//  gamma_err_ = sqrt( gamma_*gamma_/(s*s)*s_err*s_err + gamma_*gamma_/(d*d)*d_err*d_err + 9.*gamma_*gamma_/(4.*phi*phi)*phi_err*phi_err ); // propagazione
-
-  }
-
-} 
-
-
-
-
-TGraphErrors* IVScanFN::getFNgraph() const {
-
-  return IVScanFN::getFNgraph( this->graph(), this->vMin(), this->vMax(), this->n(), this->verr() );
-
-}
-
-
-TGraphErrors* IVScanFN::getFNgraph( const IScan* scan ) {
-
-  return IVScanFN::getFNgraph( scan->graph(), scan->vMin(), scan->vMax(), scan->n(), scan->verr() );
-
-}
-
-
-
-TGraphErrors* IVScanFN::getFNgraph( TGraphErrors* graph, float vMin, float vMax, int n, float verr ) {
-
+  int color = graph->GetMarkerColor();
 
   TGraphErrors* graphFN = new TGraphErrors(0);
   graphFN->SetName( Form("grFN_%s", graph->GetName()) );
   graphFN->SetMarkerSize(1.6);
   graphFN->SetMarkerStyle(20);
+  graphFN->SetMarkerColor(color);
+  graphFN->SetLineColor(color);
 
+  float xMin = 999.;
+  float xMax = 0.;
+  float yMin = 9999.;
+  float yMax = -9999.;
 
   for( unsigned iPoint=0; iPoint<graph->GetN(); ++iPoint ) {
 
@@ -149,13 +107,29 @@ TGraphErrors* IVScanFN::getFNgraph( TGraphErrors* graph, float vMin, float vMax,
     float i_err = graph->GetErrorY( iPoint );
     i_err = i_err / sqrt(n); // uncertainty on mean
 
-    if( (v>vMin) && (v<vMax) ) {
+    if( (i>=iMin) && (i<iMax) ) {
       int iPointFN = graphFN->GetN();
-      graphFN->SetPoint     ( iPointFN, 1./v, TMath::Log( i / (v*v) ) );
+      float xFN = 1./v;
+      float yFN = TMath::Log( i / (v*v) );
+      if( xFN < xMin ) xMin = xFN ;
+      if( yFN < yMin ) yMin = yFN ;
+      if( xFN > xMax ) xMax = xFN ;
+      if( yFN > yMax ) yMax = yFN ;
+      graphFN->SetPoint     ( iPointFN, xFN, yFN );
       graphFN->SetPointError( iPointFN, verr/(v*v), i_err/i );
     }
 
   } // for
+
+  float m0 = -fabs(yMax-yMin) / (xMax-xMin);
+
+  TF1* f1_line = new TF1( Form("lineFN_%s", graphFN->GetName()), "[0]+[1]*x" );
+  f1_line->SetLineColor( color );
+  f1_line->SetLineWidth(2);
+  f1_line->SetRange( 0.1*xMin, 1.1*xMax );
+  f1_line->SetParameter( 1, m0 );
+  graphFN->Fit( f1_line, "R+" );
+
 
   return graphFN;
 
@@ -163,6 +137,21 @@ TGraphErrors* IVScanFN::getFNgraph( TGraphErrors* graph, float vMin, float vMax,
 
 
 
+
+float IVScanFN::get_gamma_and_err( float& gamma_err_tot_uncorr, float& gamma_err_tot_corr, float derrcorr ) { // derrcorr is by default = -1
+
+  TGraphErrors* graphFN = this->graphFN();
+
+  TF1* f1_line = graphFN->GetFunction(Form("lineFN_%s", graphFN->GetName()));
+
+  float gamma = this->get_gamma_and_err( gamma_err_tot_uncorr, gamma_err_tot_corr, f1_line->GetParameter(1), f1_line->GetParError(1), this->d(), this->d_err_corr() );
+
+  return gamma;
+
+}
+
+
+  
 
 float IVScanFN::get_gamma_and_err( float& gamma_err_tot_uncorr, float& gamma_err_tot_corr, float s, float s_err, float d, float derrcorr ) { // derrcorr is by default = -1
 
@@ -219,20 +208,13 @@ float IVScanFN::d_err_uncorr() {
 
 float IVScanFN::d_err_corr() {
 
-  return 0.3; // 0.1 for the syst on the position (linear shifter, see logbook_ANDROMeDa entry 24/01/22) plus 0.1 for the uncertainty on the length of the tubes
+  return 0.1; // 0.1 for the syst on the position (linear shifter, see logbook_ANDROMeDa entry 24/01/22) plus 0.1 for the uncertainty on the length of the tubes
+  //return 0.3; // 0.1 for the syst on the position (linear shifter, see logbook_ANDROMeDa entry 24/01/22) plus 0.1 for the uncertainty on the length of the tubes
 
 }
 
 
   
-
-TF1* IVScanFN::lineFN() const {
-
-  return graphFN_->GetFunction(Form("lineFN_%s", this->name().c_str()));
-
-}
-
-
 
 std::string IVScanFN::xTitleFN() {
 
