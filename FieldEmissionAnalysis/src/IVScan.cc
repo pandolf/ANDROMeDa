4d468505326f1a1fa7284c2cbc1372e96d52873d
pandolf <francesco.pandolfi@cern.ch>
2023-11-15 16:38:01 +0100

    removed vMin, vMax

--- FieldEmissionAnalysis/src/IVScan.cc
+++ FieldEmissionAnalysis/src/IVScan.cc
@@ -14,7 +14,7 @@
 
 
 
-IVScan::IVScan( const std::string& name, float scale, float xMin, float xMax ) : IScan(name, scale, xMin, xMax) {
+IVScan::IVScan( const std::string& name, float scale ) : IScan(name, scale) {
 
 }
 
@@ -30,6 +30,8 @@ TGraphErrors* IVScan::graph_vsE() const {
   TGraphErrors* graph_vsE = new TGraphErrors(0);
   graph_vsE->SetName( Form("gr_E_%s", this->name().c_str()) );
   graph_vsE->SetMarkerStyle( 20 );
+  graph_vsE->SetMarkerColor( this->color() );
+  graph_vsE->SetLineColor  ( this->color() );
 
   for( unsigned iPoint=0; iPoint<this->graph()->GetN(); ++iPoint ) {
 
