5e416e25e7726733ac7ae4adcdb709f66bc49a99
pandolf <francesco.pandolfi@cern.ch>
2024-03-15 15:23:01 +0100

    added sample names

--- CommonTools/src/AndCommon.cc
+++ CommonTools/src/AndCommon.cc
@@ -516,10 +516,12 @@ std::string AndCommon::cuteSampleName( const std::string& sampleName ) {
 
   if( sampleName == "CNTArO2Etching_N1new" ) 
     cuteSampleName = "CNT mild etch";
-  else if( sampleName == "CNTArO2Etching_N1new_B" ) 
+  else if( sampleName == "CNTArO2Etching_N1new_B" || sampleName == "CNTArO2Etching_N1new_b" ) 
     cuteSampleName = "CNT mild etch B";
   else if( sampleName == "CNTArO2Etching_AsGrown" ) 
     cuteSampleName = "CNT as grown";
+  else if( sampleName == "CNTArO2Etching_Strongnew" ) 
+    cuteSampleName = "CNT strong etch";
     //cuteSampleName = "CNT mild Ar/O_{2} etching";
   
 
