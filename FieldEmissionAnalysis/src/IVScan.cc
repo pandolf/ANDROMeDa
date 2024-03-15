c115d2c093becfd202f6c9d6d6b96afcfdb5eb2b
pandolf <francesco.pandolfi@cern.ch>
2024-03-15 15:25:40 +0100

    added copy constructor

--- FieldEmissionAnalysis/src/IVScan.cc
+++ FieldEmissionAnalysis/src/IVScan.cc
@@ -19,10 +19,41 @@ IVScan::IVScan( const std::string& name, float scale ) : IScan(name, scale) {
 }
 
 
+IVScan::IVScan( const IVScan& scan ) : IScan(scan) {
+
+}
+
+
 IVScan::~IVScan() {
 
 }
 
+
+void IVScan::add_deltaV( float deltaV ) {
+
+  TGraphErrors* graph = this->graph();
+//std::string name(graph->GetName());
+//graph->SetName("gr_tmp");
+
+//TGraphErrors* graph_deltaV = new TGraphErrors(0);
+//graph_deltaV->SetName( name.c_str() );
+
+  for( unsigned iPoint=0; iPoint < graph->GetN(); ++iPoint ) {
+
+    double x, y;
+    graph->GetPoint( iPoint, x, y );
+    double xerr = this->graph()->GetErrorX( iPoint );
+    double yerr = this->graph()->GetErrorY( iPoint );
+
+    graph->SetPoint     ( iPoint, x+deltaV, y );
+    graph->SetPointError( iPoint, xerr, yerr );
+
+  } // for points
+
+  this->set_graph( graph );
+  //delete graph;
+
+}
 
 
 TGraphErrors* IVScan::graph_vsE() const {
@@ -41,7 +72,7 @@ TGraphErrors* IVScan::graph_vsE() const {
     double yerr = this->graph()->GetErrorY( iPoint );
 
     graph_vsE->SetPoint( iPoint, x/this->d(), y );
-    graph_vsE->SetPointError( iPoint, xerr/this->d(), yerr );
+    graph_vsE->SetPointError( iPoint, xerr/this->d(), yerr/sqrt(this->n()) );
 
   } // for points
 
