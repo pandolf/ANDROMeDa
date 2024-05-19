984bafe1b0877edd3ba624ccc8aa38021b3884c6
Francesco Pandolfi <pandolf@pMacbookProM3-2.local>
2024-05-19 16:18:58 +0200

    first working version compatible with script

--- FieldEmissionAnalysis/analysis/makeRawTree.cpp
+++ FieldEmissionAnalysis/analysis/makeRawTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 #include "lecroyparser.h"
 #include "AndCommon.h"
@@ -9,34 +10,62 @@
 
 
 
-int main() {
+int main( int argc, char* argv[] ) {
 
-  int cd = 188;
-  int v = 104;
 
-  std::string dir_trc(Form("data/trc/CD%d/%d V", cd, v));
-  std::string fileName_trc("C1--PTL4eD_CNT_StrongEtching_mini_40_mK_51p841uA_m166mV_trg_104V--00057.trc");
+  if( argc != 2 ) {
+
+    std::cout << "USAGE: ./makeRawTree [fullpath_to_trcfile]" << std::endl;
+    std::cout << "EXAMPLE: ./makeRawTree data/trc/CD188/104V/C1--PTL4eD_CNT_StrongEtching_mini_40_mK_51p841uA_m166mV_trg_104V--00057.trc" << std::endl;
+    exit(1);
+
+  }
+
+  std::string fullPath(argv[1]);
+  std::vector<std::string> dirs = AndCommon::splitString( fullPath, "/" );
+
+
+  int cd = atoi(AndCommon::splitString(dirs[2],"CD")[0].c_str());
+  int v  = atoi(AndCommon::splitString(dirs[3], "V")[0].c_str());
+
+
+  std::string dir_trc(Form("data/trc/CD%d/%dV", cd, v));
+  std::string fileName_trc(dirs[dirs.size()-1]);
 
   std::string fileName = AndCommon::splitString( fileName_trc, "." )[0];
-  //std::string fileName_root = fileName + ".root";
 
-  TFile* file = TFile::Open( "test.root", "recreate" );
-  //TFile* file = TFile::Open( Form("%s/%s.root", dir.c_str(), fileName.c_str()), "recreate" );
+  int lumiblock = atoi(AndCommon::splitString(fileName,"--")[2].c_str());
 
-  TGraphErrors* gr = new TGraphErrors(0);
+  std::string outdir( Form("data/root/CD%d/%dV", cd, v));
+  system( Form("mkdir -p %s", outdir.c_str()) );
+
+  TFile* outfile = TFile::Open( Form("%s/%s.root", outdir.c_str(), fileName.c_str()), "recreate" );
 
   TTree* tree = new TTree("treeraw", "");
 
   tree->Branch( "CD_number", &cd, "CD_number/I" );
+  tree->Branch( "voltage"  , &v , "voltage/I"   );
 
   int nEvent = 0;
   tree->Branch( "event", &nEvent, "nEvent/I" );
+  tree->Branch( "lumi" , &lumiblock, "lumi/I" );
+
+  double trigtime_d;
+  tree->Branch( "trigtime_d", &trigtime_d, "trigtime_d/D" );
+  double trigtime_h;
+  tree->Branch( "trigtime_h", &trigtime_h, "trigtime_h/D" );
+  double trigtime_m;
+  tree->Branch( "trigtime_m", &trigtime_m, "trigtime_m/D" );
+  double trigtime_s;
+  tree->Branch( "trigtime_s", &trigtime_s, "trigtime_s/D" );
 
   float pshape[2502];
   tree->Branch( "pshape", pshape, "pshape[2502]/F" );
 
   lecroyparser::lecroy_wavedesc_2_3 header;
   int16_t* waveform = nullptr;
+
+  std::cout << "-> Reading trc file: " << fullPath << std::endl;
 
   bool res = lecroyparser::read(Form("%s/%s", dir_trc.c_str(), fileName_trc.c_str()), header, waveform);
 
@@ -46,6 +75,8 @@ int main() {
 
     int nEvents_thisFile = header.wave_array_count/nsamples;
 
+    std::cout << "-> This file has: " << nEvents_thisFile << " events" << std::endl;
+
     for( unsigned iEv=0; iEv<nEvents_thisFile; ++iEv ) {
 
       for(uint16_t i = 0; i < nsamples; ++i ) {
@@ -54,6 +85,12 @@ int main() {
         //gr->SetPoint(i, i, waveform[i]*header.vertical_gain-header.vertical_offset);
         //std::cout << i << "\t" << waveform[i] << "\t" << i*header.horiz_offset+header.horiz_interval << " " << waveform[i]*header.vertical_gain-header.vertical_offset << " " << std::endl;
       }
+
+      trigtime_d = header.trigger_time.days*24.*60.*60.;
+      trigtime_h = header.trigger_time.hours*60.*60.;
+      trigtime_m = header.trigger_time.minutes*60.;
+      trigtime_s = header.trigger_time.seconds;
+                 
 
       tree->Fill();
       nEvent++;
@@ -70,9 +107,11 @@ int main() {
 
   }
 
-  file->cd();
+  outfile->cd();
   tree->Write();
-  file->Close();
+  outfile->Close();
+
+  std::cout << "-> Saved raw tree in: " << outfile->GetName() << std::endl;
 
   return 0;
 
