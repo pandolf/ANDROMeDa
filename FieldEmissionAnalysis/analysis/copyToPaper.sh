d1b756070bc1b2fadc48994c8f99fcb29d3b9cfb
Francesco Pandolfi <pandolf@pMacbookProM3-2.local>
2025-03-19 15:33:59 +0100

    new paper

--- FieldEmissionAnalysis/analysis/copyToPaper.sh
+++ FieldEmissionAnalysis/analysis/copyToPaper.sh
@@ -1,5 +1,4 @@
-cp plots/GammaScans/CNTArO2Etching_AG_old/i_vs_v.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/i_vs_v_CNTArO2Etching_AG_old.pdf
-cp plots/GammaScans/CNTArO2Etching_N1/i_vs_v.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/i_vs_v_CNTArO2Etching_N1.pdf
-cp plots/GammaScans/CNTArO2Etching_AG_old/fn.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/fn_CNTArO2Etching_AG_old.pdf
-cp plots/GammaScans/CNTArO2Etching_N1/fn.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/fn_CNTArO2Etching_N1.pdf
-cp plots/GammaScans/CNTArO2Etching_N1/chi2Scan.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/chi2Scan_CNTArO2Etching_N1.pdf
+cp iv_paper.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/
+cp fn_paper.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/
+cp gamma_vs_r.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/
+cp radiusHisto.pdf ~/FISICA/Nanotubes/NanoUV/papers/Field\ Emission\ and\ Etching/figures/
